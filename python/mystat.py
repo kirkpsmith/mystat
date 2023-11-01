9badefd1372defa8c5a2b5424b90b1d6b1aa05dc
Kirk Smith <kirkpsmith@noreply.codeberg.org>
2023-11-01 20:57:20 +0000

    Update python/mystat.py

--- python/mystat.py
+++ python/mystat.py
@@ -5,7 +5,7 @@
 # calibration, and three pre-programmed measurement methods geared towards battery research (staircase cyclic voltammetry, constant-current charge/discharge, and rate testing).
 # It is cross-platform, requiring only a working installation of Python 3.x together with the Numpy, Scipy, PyUSB, and PyQtGraph packages.
 
-# Author: Thomas Dobbelaere, modified by Matthew Yates and Paul Irving
+# Author: Thomas Dobbelaere, modified by Matthew Yates, Paul Irving, Daniel Fernandez Pinto, Kirk Smith
 # License: GPL
 from PyQt5 import QtWidgets, QtGui, QtCore
 import pyqtgraph
